cd8bc11796382af9468d9c1a8f440ea4ab6769d8
gzingle <gzingle@ncsu.edu>
2018-04-21 10:21:28 -0400

    Fixed naming error

--- PackHacks/src/packHacks/sports_manager/ui/SportsManagerUI.java
+++ PackHacks/src/packHacks/sports_manager/ui/SportsManagerUI.java
@@ -1,9 +1,9 @@
-package packHakcs.sports_manager.ui;
+package packHacks.sports_manager.ui;
 
 public class SportsManagerUI {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("TEST");
 	}
 
 }
